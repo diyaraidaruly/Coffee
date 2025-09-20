8b3c93e35b334aa222c697de388679e309ca4db3
Aidaruly Diyar <aidaruly2005@gmail.com>
2025-09-20 14:58:35 +0500

    new

---
+++ js/CoffeeProject.js
@@ -0,0 +1,27 @@
+function showPage(pageId) {
+    document.querySelectorAll('section').forEach(s => s.style.display = 'none');
+    const page = document.getElementById(pageId);
+    if (page) page.style.display = 'block';
+}
+document.addEventListener('DOMContentLoaded', () => showPage('about'));
+document.addEventListener("DOMContentLoaded", () => {
+    const tooltip = document.createElement("div");
+    tooltip.className = "tooltip-box";
+    document.body.appendChild(tooltip);
+
+    document.querySelectorAll(".has-tooltip").forEach(el => {
+        el.addEventListener("mouseenter", () => {
+            tooltip.textContent = el.dataset.tooltip;
+            const rect = el.getBoundingClientRect(); // элементтің орны
+            tooltip.style.left = rect.left + window.scrollX + rect.width / 2 - tooltip.offsetWidth / 2 + "px";
+            tooltip.style.top = rect.bottom + window.scrollY + 8 + "px"; // астына +8px
+            tooltip.style.opacity = "1";
+            tooltip.style.transform = "translateY(0)";
+        });
+
+        el.addEventListener("mouseleave", () => {
+            tooltip.style.opacity = "0";
+            tooltip.style.transform = "translateY(5px)";
+        });
+    });
+});
